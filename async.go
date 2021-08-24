e94e2396cd2cd573d42b9292fedb2524a6af2161
unknown <amirsil2002@gmail.com>
2021-08-25 02:46:11 +0300

    Add awaitAll functionality and arguments support for async functions

--- async.go
+++ async.go
@@ -7,15 +7,16 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"sync"
 	"time"
 )
 
-func async(f func() interface{}) <-chan interface{} {
+func async(f func(...interface{}) interface{}, args ...interface{}) <-chan interface{} {
 	pipe := make(chan interface{}, 1)
 
 	go func() {
 		defer close(pipe)
-		pipe <- f()
+		pipe <- f(args...)
 	}()
 
 	return pipe
@@ -25,27 +26,63 @@ func await(awaitable <-chan interface{}) interface{} {
 	return <-awaitable
 }
 
-func getNumber() interface{} {
+func awaitAll(awaitables ...<-chan interface{}) []interface{} {
+	results := make([]interface{}, len(awaitables))
+	wg := new(sync.WaitGroup)
+
+	for index, awaitable := range awaitables {
+		wg.Add(1)
+		// index and awaitable could change before the goroutine is over
+		staticIndex, staticAwaitable := index, awaitable
+		go func() {
+			defer wg.Done()
+			results[staticIndex] = <-staticAwaitable
+		}()
+
+	}
+	wg.Wait()
+	return results
+}
+
+// randWithDelay(delay int) (int)
+func randWithDelay(args ...interface{}) interface{} {
+	delay := time.Duration(args[0].(int))
+
 	rand.Seed(time.Now().UnixNano())
-	time.Sleep(5 * time.Second)
+	time.Sleep(delay * time.Second)
 	return rand.Int()
 }
 
-func getJoke() interface{} {
+// getJokeFromAPI() (string)
+func getJokeFromAPI(args ...interface{}) interface{} {
 	httpResp, err := http.Get("https://v2.jokeapi.dev/joke/Any")
 	if err != nil {
 		log.Fatal(err)
 	}
 	bodyBytes, err := ioutil.ReadAll(httpResp.Body)
-	var result map[string]interface{}
-	json.Unmarshal([]byte(string(bodyBytes)), &result)
-	return fmt.Sprintf("%v\nanswer: %v", result["setup"], result["delivery"])
+	var response map[string]interface{}
+	json.Unmarshal([]byte(string(bodyBytes)), &response)
+
+	var joke string
+	if _, ok := response["joke"]; ok {
+		joke = fmt.Sprintf("\njoke: %v", response["joke"])
+	} else {
+		joke = fmt.Sprintf("\nquestion: %v\nanswer: %v", response["setup"], response["delivery"])
+	}
+
+	return joke
 }
 
 func main() {
-	randNum := async(getNumber)
-	joke := async(getJoke)
+	randNum := async(randWithDelay, 3)
+	joke := async(getJokeFromAPI)
 
-	fmt.Printf("%v\n", await(joke))
-	fmt.Printf("random number: %v\n", await(randNum))
+	results := awaitAll(randNum, joke)
+	for index, result := range results {
+		fmt.Printf("%v: %v\n", index+1, result)
+	}
+
+	time.Sleep(1)
+	fmt.Printf("%v\n", await(async(getJokeFromAPI)))
+	fmt.Printf("%v\n", await(async(randWithDelay, 2)))
 }
